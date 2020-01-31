f6e481070a819614a58cfbc460b5587b96a63a34
Fredrik <fredrik.enges@luckan.fi>
2020-01-31 12:14:19 +0200

    edit

--- blocks/block-dual.php
+++ blocks/block-dual.php
@@ -1,11 +1,11 @@
-<div  class="hero-fiftycontainer alignfull nomarginhero" 
+  <div  class="hero-fiftycontainer alignfull nomarginhero" 
       style="background-color:<?php block_field( 'hero-bgcolor' ); ?> !important;
             flex-direction: <?php block_field( 'hero-flip-order' ); ?>;">
-  <div class="hero-left-side">
-    <article class="left-text">
-      <h1><?php block_field( 'hero-titel' ); ?></h1>
-      <p><?php block_field( 'hero-undertext' ); ?></p>
-    </article>
+    <div class="hero-left-side">
+      <article class="left-text">
+       <h1><?php block_field( 'hero-titel' ); ?></h1>
+       <p><?php block_field( 'hero-undertext' ); ?></p>
+     </article>
   </div>
     
      <img class="right_image right-half" 
@@ -13,4 +13,4 @@
               style=
               "object-fit: <?php block_field( 'toggle-contain-image' ); ?>;
               padding: <?php block_field( 'margin-image' ); ?>px;"> 
-</div>
+  </div>
