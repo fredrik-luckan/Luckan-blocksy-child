c4cc7157d8d9a7b952d033edbc248ab4c2ce9ee1
Fredrik <fredrik.enges@luckan.fi>
2020-02-03 15:48:22 +0200

    Update block-dual.php

--- blocks/block-dual.php
+++ blocks/block-dual.php
@@ -3,6 +3,7 @@
             flex-direction: <?php block_field( 'hero-flip-order' ); ?>;">
     <div class="hero-left-side">
       <article class="left-text">
+       <<?php block_field( 'header-type' ); ?>> <?php block_field( 'hero-titel' ); ?> <<?php block_field( 'header-type' , "/" ); ?>>
        <p><?php block_field( 'hero-undertext' ); ?></p>
      </article>
   </div>
@@ -14,4 +15,3 @@
               padding: <?php block_field( 'margin-image' ); ?>px;"> 
   </div>
 
-  
