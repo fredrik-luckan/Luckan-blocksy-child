d68e2a216e32c9000182e71cdd4b2e74fcb6686e
Fredrik <fredrik.enges@luckan.fi>
2020-02-04 09:34:35 +0200

    Update block-dual.php

--- blocks/block-dual.php
+++ blocks/block-dual.php
@@ -8,14 +8,15 @@
      </article>
   </div>
     
-  <img class="right_image right-half" 
+  <div class="right_image right-half" 
               style="
               object-fit: <?php block_field( 'toggle-contain-image' ); ?>;
               padding: <?php block_field( 'margin-image' ); ?>px;
-              " 
+              ">
 
-              src= <?php
-                    $attachment_id = block_value( 'hero-half-image' );
-                    echo wp_get_attachment_image( $attachment_id, 'large' );?> >
+              <?php
+                $attachment_id = block_value( 'hero-half-image' );
+                echo wp_get_attachment_image( $attachment_id, 'large' );?> 
+  </div>
   </div>
 
