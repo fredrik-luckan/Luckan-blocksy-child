c859c473a715ca76492028789fb733b616827175
Fredrik <fredrik.enges@luckan.fi>
2020-03-05 14:58:21 +0200

    Update block-dual.php

--- blocks/block-dual.php
+++ blocks/block-dual.php
@@ -11,14 +11,12 @@
                 <?php $button_text1 = block_value('button-1-text'); ?>
             <a class="block-hero_button-1 <?php echo empty($button_text1) ? 'hide' : ''; ?>" 
                 href="<?php block_field('button-1-url'); ?>">
-                <?php block_field('button-1-text'); ?> 
                 <?php echo $button_text1; ?>
             </a>
               
             <?php $button_text1 = block_value('button-2text'); ?>
             <a class="block-hero_button-2 <?php echo empty($button_text2) ? 'hide' : ''; ?>" 
             href="<?php block_field('button-2-url'); ?>">
-            <?php block_field('button-2-text'); ?> 
                 <?php echo $button_text2; ?>
 </a>
 
